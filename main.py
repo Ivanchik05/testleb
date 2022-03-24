0764ab1553bc38a3b5c6b8d74b2594dea9a33fff
Ivanchik05 <ivan.bozvanoff@yandex.ru>
2022-03-24 20:29:52 +0300

    edit main

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template, redirect
 from flask_login import LoginManager, login_user, login_required, logout_user
 from data import db_session
@@ -13,7 +15,8 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 def main():
     db_session.global_init("db/bas.db")
-    app.run(port=8080, host='127.0.0.1')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 @login_manager.user_loader
